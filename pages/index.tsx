2369c10b94aa0a971ba0b7dbae5ab8ba659edcbb
Giovanna Zaccolon <102630771+gzccl22@users.noreply.github.com>
2024-04-09 13:41:33 -0300

    Add files via upload

---
+++ pages/index.tsx
@@ -0,0 +1,94 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import styles from "../styles/Home.module.css";
+
+const Home: NextPage = () => {
+  const [partyTime, setPartyTime] = useState(false);
+  const [days, setDays] = useState(0);
+  const [hours, setHours] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+
+  useEffect(() => {
+    const target = new Date("12/31/2021 23:59:59");
+
+    const interval = setInterval(() => {
+      const now = new Date();
+      const difference = target.getTime() - now.getTime();
+
+      const d = Math.floor(difference / (1000 * 60 * 60 * 24));
+      setDays(d);
+
+      const h = Math.floor(
+        (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      setHours(h);
+
+      const m = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+      setMinutes(m);
+
+      const s = Math.floor((difference % (1000 * 60)) / 1000);
+      setSeconds(s);
+
+      if (d <= 0 && h <= 0 && m <= 0 && s <= 0) {
+        setPartyTime(true);
+      }
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Contagem regressiva</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      {partyTime ? (
+        <>
+          <h1>Feliz ano novo!</h1>
+          <video autoPlay loop muted>
+            <source src="/party.mp4" />
+          </video>
+        </>
+      ) : (
+        <>
+          <div className="timer-wrapper">
+            <div className="timer-inner">
+              <div className="timer-segment">
+                <span className="time">{days}</span>
+                <span className="label">Days</span>
+              </div>
+              <span className="divider">:</span>
+              <div className="timer-segment">
+                <span className="time">{hours}</span>
+                <span className="label">Horas</span>
+              </div>
+              <span className="divider">:</span>
+              <div className="timer-segment">
+                <span className="time">{minutes}</span>
+                <span className="label">Minutos</span>
+              </div>
+              <span className="divider">:</span>
+              <div className="timer-segment">
+                <span className="time">{seconds}</span>
+                <span className="label">Segundos</span>
+              </div>
+            </div>
+          </div>
+          <Image
+            alt="background image"
+            src="/image.webp"
+            layout="fill"
+            quality={100}
+          />
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Home;
